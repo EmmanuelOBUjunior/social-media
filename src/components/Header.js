8bc481c4a0c3f09c88b767accf6e732a3d087a1f
EmmanuelOBUjunior <obu.junior.emmanuel>
2021-10-19 19:41:41 -0700

    Styling Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import SearchIcon from '@material-ui/icons/Search'
 import HomeIcon from '@material-ui/icons/Home'
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {IconButton} from '@material-ui/core';
+import "./Header.css"
 
 const Header = () => {
     return (
@@ -8,13 +13,28 @@ const Header = () => {
             <div className = "header__left">
                 <img src = " " alt = "Logo"/>
             </div>
-            <div className = "header__input">
+            <div className = "header__input header__input-center">
                  <SearchIcon/>
                 <input placeholder = "Search Social Media" type = "text"/>
             </div>
-            <div className = "header__center">
-                <div className="header__option header__option--active">
+            <div className = "header__right">
+            <div className="header__option header__option--active">
+                    
                     <HomeIcon fontSize = "large" />
+                   
+                </div>
+                <div className="header__option">
+                    
+                    <ChatIcon fontSize = "large"/>
+                </div>
+                <div className="header__option">
+                    
+                    <NotificationsIcon fontSize = "large"/>
+                </div>
+                <div className="header__option">
+                    
+                    <AccountCircleIcon fontSize = "large" color = "primary"/>
+                    <h4>Emmanuel Obu Junior</h4>
                 </div>
             </div>
         </div>
